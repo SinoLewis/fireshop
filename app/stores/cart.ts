23ba56cf0541b1a2d0043fc16bdd6e55c00984d0
SinoLewis <sinolewis@gmail.com>
2023-01-21 20:55:24 +0300

    Custom store for cart functionality

--- app/stores/cart.ts
+++ app/stores/cart.ts
@@ -61,3 +61,63 @@ function getCart(): Cart {
   return JSON.parse(decrypt(localStorage.getItem(SELECTED_CART)));
 }
 const cart = writable<Cart>(getCart());
+
+const add = async (name: string) => {
+  let { id, title, price } = products[name];
+  cart.update((value) => {
+    if (value.cart_products.hasOwnProperty(title)) {
+      value.cart_products[title].quantity++;
+    } else {
+      value.cart_products[title] = {
+        id,
+        price,
+        quantity: 1,
+        total_price: price,
+      };
+    }
+    return { ...value };
+  });
+};
+const minus = async (name: string) => {
+  let item = products[name];
+  cart.update((value) => {
+    if (value.cart_products.hasOwnProperty(item.title)) {
+      value.cart_products[item.title].quantity--;
+      if (value.cart_products[item.title].quantity === 0)
+        delete value.cart_products[item.title];
+    }
+    return { ...value };
+  });
+};
+const remove = async (name: string) => {
+  let item = products[name];
+  cart.update((value) => {
+    delete value.cart_products[item.title];
+    return { ...value };
+  });
+};
+
+cart.subscribe((value) => {
+  Object.values(value.cart_products).forEach((item) => {
+    // TODO: item price, cart price, cart total
+    item.total_price = item.price * item.quantity;
+  });
+  value.cart_price = Object.values(value.cart_products).reduce(
+    (acc, item) => acc + item.total_price,
+    0
+  );
+  value.cart_total = Object.values(value.cart_products).reduce(
+    (acc, item) => acc + item.quantity,
+    0
+  );
+  let date = new Date();
+  value.updated_at = date.toLocaleString("en-US", {
+    dateStyle: "full",
+    timeStyle: "full",
+  });
+  console.log("TT PRICE", value.cart_price, "\n$CART", value);
+  const SELECTED_CART: any = import.meta.env.VITE_LOCAL_CART;
+  localStorage.setItem(SELECTED_CART, encrypt(JSON.stringify(value)));
+});
+
+export { cart, add, minus, remove };
