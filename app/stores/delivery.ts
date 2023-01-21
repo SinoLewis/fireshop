b83cddabefefbd4af827277e282fa8a3a8a07655
SinoLewis <sinolewis@gmail.com>
2023-01-21 21:05:31 +0300

    Custom delivery store for glovo delivery API.

--- app/stores/delivery.ts
+++ app/stores/delivery.ts
@@ -133,3 +133,299 @@ const parcel = writable<Parcel>(null);
 const courier = writable<Courier>(null);
 // TODO: DEV ONLY
 const simulate = writable<Simulate>(null);
+
+async function clearCart() {
+  const SELECTED_CART: any = import.meta.env.VITE_LOCAL_CART;
+  localStorage.removeItem(SELECTED_CART);
+}
+const createParcel = async (body: Parcel) => {
+  try {
+    let response = await fetch("http://localhost:3000/parcel", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ body }),
+    });
+    let data: Parcel = await response.json();
+    if (response.status === 200 || 201) {
+      parcel.set(data);
+      clearCart();
+      toast.set({
+        icon: "🚚",
+        message: "Your Delivery is on the way!",
+        type: "success",
+      });
+      console.log("POST: ", data);
+      return data;
+    } else {
+      toast.set({
+        icon: "🚫",
+        message: "Delivery error! Confirm your details well.",
+        type: "error",
+      });
+      console.log(`POST: ${response.status}: ${response.statusText}`);
+    }
+  } catch (error) {
+    console.error("POST: ", error.message);
+    return null;
+  }
+};
+const verifyParcel = async (
+  customerAddress: CustomerAddress,
+  pickupDetails: PickupAddress
+) => {
+  try {
+    let response = await fetch("http://localhost:3000/parcel/verify", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ customerAddress, pickupDetails }),
+    });
+    let data: VerifyParcel = await response.json();
+    if (response.status === 200 || 201) {
+      parcel.update((value) => {
+        return {
+          ...value,
+          pickupDetails: data.pickupDetails,
+          address: data.address,
+        };
+      });
+      console.log("POST: ", data);
+      return data;
+    } else {
+      console.log(`POST: ${response.status}: ${response.statusText}`);
+    }
+  } catch (error) {
+    console.error("POST: ", error.message);
+    return null;
+  }
+};
+const cancelParcel = async (trackingNumber: string) => {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/parcel/${trackingNumber}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    if (response.status === 200 || 201) {
+      parcel.set(null);
+      console.log("GET: ", data);
+    } else {
+      console.log(`ERROR: ${response.status}: ${response.statusText}`);
+      return { code: response.status, description: response.statusText };
+    }
+  } catch (error) {
+    console.error("GET: ", error.message);
+    return null;
+  }
+};
+const statusParcel = async (
+  trackingNumber: string
+): Promise<StateChangeHistory> => {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/parcel/${trackingNumber}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    if (response.status === 200 || 201) {
+      parcel.set(data);
+      console.log("GET: ", data);
+      return data.stateChangeHistory;
+    } else {
+      console.log(`ERROR: ${response.status}: ${response.statusText}`);
+    }
+  } catch (error) {
+    console.error("GET: ", error.message);
+    return null;
+  }
+};
+const infoCourier = async (trackingNumber: string): Promise<Asignee> => {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/courier/${trackingNumber}/info`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    if (response.status === 200 || 201) {
+      courier.update((value) => {
+        return { ...value, assignedCourier: data.assignedCourier };
+      });
+      console.log("GET: ", data);
+      return data.assignedCourier;
+    } else {
+      console.log(`ERROR: ${response.status}: ${response.statusText}`);
+    }
+  } catch (error) {
+    console.error("GET: ", error.message);
+    return null;
+  }
+};
+const postionCourier = async (trackingNumber: string): Promise<Position> => {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/courier/${trackingNumber}/position`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    if (response.status === 200 || 201) {
+      courier.update((value) => {
+        return { ...value, position: data.position };
+      });
+      console.log("GET: ", data);
+      return data.position;
+    } else {
+      console.log(`ERROR: ${response.status}: ${response.statusText}`);
+    }
+  } catch (error) {
+    console.error("GET: ", error.message);
+    return null;
+  }
+};
+
+const trackLink = async (trackingNumber: string): Promise<string> => {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/courier/${trackingNumber}/link`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    if (response.status === 200 || 201) {
+      courier.update((value) => {
+        return { ...value, link: data.link };
+      });
+      console.log("GET: ", data);
+      return data.link;
+    } else {
+      console.log(`ERROR: ${response.status}: ${response.statusText}`);
+    }
+  } catch (error) {
+    console.error("GET: ", error.message);
+    return null;
+  }
+};
+
+const simSuccess = async (trackingNumber: string) => {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/sim/${trackingNumber}/success`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    if (response.status === 200 || 201) {
+      simulate.update((value) => {
+        return { ...value, success: data.success };
+      });
+      console.log("GET: ", data);
+      return data.success;
+    } else {
+      console.log(`ERROR: ${response.status}: ${response.statusText}`);
+    }
+  } catch (error) {
+    console.error("GET: ", error.message);
+    return null;
+  }
+};
+const simFail = async (trackingNumber: string) => {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/sim/${trackingNumber}/fail`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    if (response.status === 200 || 201) {
+      simulate.update((value) => {
+        return { ...value, fail: data.fail };
+      });
+      console.log("GET: ", data);
+      return data;
+    } else {
+      console.log(`ERROR: ${response.status}: ${response.statusText}`);
+    }
+  } catch (error) {
+    console.error("GET: ", error.message);
+    return null;
+  }
+};
+
+const testPost = async (user) => {
+  try {
+    const response = await fetch("http://localhost:3000/users/21", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ user }),
+    });
+    const data = await response.json();
+    if (response.status === 200 || 201) {
+      console.log("GET: ", data);
+    } else {
+      console.log(`ERROR: ${response.status}: ${response.statusText}`);
+    }
+  } catch (error) {
+    console.error("GET: ", error.message);
+    return null;
+  }
+};
+
+const testGet = async () => {
+  try {
+    const response = await fetch("http://localhost:3000/users/21", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    if (response.status === 200 || 201) {
+      console.log("GET: ", data);
+      return data.position;
+    } else {
+      console.log(`ERROR: ${response.status}: ${response.statusText}`);
+    }
+  } catch (error) {
+    console.error("GET: ", error.message);
+    return null;
+  }
+};
+export { parcel, verifyParcel, createParcel, statusParcel };
+
+export type { Parcel, PickupAddress };
