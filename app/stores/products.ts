486f5b55e43ecb9ee08b86ebc2538c106688ce1a
SinoLewis <sinolewis@gmail.com>
2023-01-21 20:59:09 +0300

    Dummy products data for the Shop

---
+++ app/stores/products.ts
@@ -0,0 +1,152 @@
+export const products = {
+  "Huawei P30": {
+    id: "9dfcb288-3517-4fa0-ac24-c8e446ccfc61",
+    title: "Huawei P30",
+    price: 499,
+  },
+  "Eau De Perfume Spray": {
+    id: "ea80bd0c-1991-4528-8371-b284ce5bc223",
+    title: "Eau De Perfume Spray",
+    price: 30,
+  },
+  "OPPOF19": {
+    id: "d153138b-8bbe-4606-aa60-0babcc529186",
+    title: "OPPOF19",
+    price: 280,
+  },
+  "Hyaluronic Acid Serum": {
+    id: "4b195327-2353-44fd-ba0a-d9754ed91e10",
+    title: "Hyaluronic Acid Serum",
+    price: 19,
+  },
+  "perfume Oil": {
+    id: "79c560db-a2c3-472d-89f4-e879c92ae351",
+    title: "perfume Oil",
+    price: 13,
+  },
+  "Microsoft Surface Laptop 4": {
+    id: "cd56a088-9f6b-4924-ac22-e966f2c75f96",
+    title: "Microsoft Surface Laptop 4",
+    price: 1499,
+  },
+  "Elbow Macaroni - 400 gm": {
+    id: "10e65a7a-a807-484e-a287-b247dadfaefb",
+    title: "Elbow Macaroni - 400 gm",
+    price: 14,
+  },
+  "MacBook Pro": {
+    id: "2d62d245-da7d-4d2a-91b1-740ebb3b3811",
+    title: "MacBook Pro",
+    price: 1749,
+  },
+  "Plant Hanger For Home": {
+    id: "44e62bf4-bd8c-473b-aab5-7bd5ff2aa4d0",
+    title: "Plant Hanger For Home",
+    price: 41,
+  },
+  "Samsung Galaxy Book": {
+    id: "ee2eafdd-c6a9-4af6-a61b-49df57770649",
+    title: "Samsung Galaxy Book",
+    price: 1499,
+  },
+  "Brown Perfume": {
+    id: "8d6be598-fe22-4acf-8a19-759d71130800",
+    title: "Brown Perfume",
+    price: 40,
+  },
+  "Orange Essence Food Flavou": {
+    id: "bc376d8e-2ebd-439f-9ba7-f5bd287e9fef",
+    title: "Orange Essence Food Flavou",
+    price: 14,
+  },
+  "cereals muesli fruit nuts": {
+    id: "07416722-e63c-4337-b3db-910f2756b454",
+    title: "cereals muesli fruit nuts",
+    price: 46,
+  },
+  "Samsung Universe 9": {
+    id: "784fc630-db13-471c-b79a-daa5730f7dcc",
+    title: "Samsung Universe 9",
+    price: 1249,
+  },
+  "Infinix INBOOK": {
+    id: "5d0f52dc-3e33-43b6-95fd-59e3aa8c1fac",
+    title: "Infinix INBOOK",
+    price: 1099,
+  },
+  "Fog Scent Xpressio Perfume": {
+    id: "ffbfb875-98d0-4898-b8a3-52038d85881f",
+    title: "Fog Scent Xpressio Perfume",
+    price: 13,
+  },
+  "Handcraft Chinese style": {
+    id: "d3f84c3f-2faa-44f6-958c-8474f28b6024",
+    title: "Handcraft Chinese style",
+    price: 60,
+  },
+  "Key Holder": {
+    id: "4af6e5e3-88e6-4cc4-9b72-1db15db979f7",
+    title: "Key Holder",
+    price: 30,
+  },
+  "Non-Alcoholic Concentrated Perfume Oil": {
+    id: "81ba804e-bd0d-410f-ac12-562ed07a0a2d",
+    title: "Non-Alcoholic Concentrated Perfume Oil",
+    price: 120,
+  },
+  "iPhone X": {
+    id: "9bd20953-3929-41a8-a493-7a428a250b32",
+    title: "iPhone X",
+    price: 899,
+  },
+  "Gulab Powder 50 Gram": {
+    id: "eafa83bb-88b6-4a9a-bb45-8d8e51bfaa55",
+    title: "Gulab Powder 50 Gram",
+    price: 70,
+  },
+  "Flying Wooden Bird": {
+    id: "bcaea94f-93b2-4686-8e1a-2112b4e1137f",
+    title: "Flying Wooden Bird",
+    price: 51,
+  },
+  "iPhone 9": {
+    id: "687bcc49-3dda-4730-aa13-36a16899e196",
+    title: "iPhone 9",
+    price: 549,
+  },
+  "Freckle Treatment Cream- 15gm": {
+    id: "1687f517-ae57-433d-91a1-9f36da0664c5",
+    title: "Freckle Treatment Cream- 15gm",
+    price: 70,
+  },
+  "HP Pavilion 15-DK1056WM": {
+    id: "1db28ca0-cb1c-4e13-a63a-4e8c69124991",
+    title: "HP Pavilion 15-DK1056WM",
+    price: 1099,
+  },
+  "3D Embellishment Art Lamp": {
+    id: "0bb0c514-2531-495b-94bf-484a252433b5",
+    title: "3D Embellishment Art Lamp",
+    price: 20,
+  },
+  "Oil Free Moisturizer 100ml": {
+    id: "cc47fd32-38db-4b4a-a7de-011ed0063547",
+    title: "Oil Free Moisturizer 100ml",
+    price: 40,
+  },
+  "Skin Beauty Serum.": {
+    id: "c0ae66b6-31f7-4e13-a910-e9657fc6d031",
+    title: "Skin Beauty Serum.",
+    price: 46,
+  },
+  "- Daal Masoor 500 grams": {
+    id: "73ca2d4c-43a4-47e0-a4bd-12ca68ed61d3",
+    title: "- Daal Masoor 500 grams",
+    price: 20,
+  },
+  "Tree Oil 30ml": {
+    id: "6593909c-30ce-447b-aa85-c100fcb3da9a",
+    title: "Tree Oil 30ml",
+    price: 12,
+  },
+};
