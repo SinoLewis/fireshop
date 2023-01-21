90ee9a2666b6656cc7b9b14f2ec067ed6e8b8f07
SinoLewis <sinolewis@gmail.com>
2023-01-21 22:53:29 +0300

    User Auth compo init

---
+++ app/components/shop/no-user.svelte
@@ -0,0 +1,6 @@
+<svelte:options tag="no-user" />
+
+<h2>You're signed out 😞... Sign in To use this Page</h2>
+<modal-action type="open" name="signin">
+  <button>Open Login</button>
+</modal-action>
