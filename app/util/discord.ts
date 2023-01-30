47996bdc82338ad4b80c851def0890b61af1115b
SinoLewis <sinolewis@gmail.com>
2023-01-30 04:01:19 +0300

    errors with user id

--- app/util/discord.ts
+++ app/util/discord.ts
@@ -1,20 +1,29 @@
+import { user } from "../stores";
+
 export async function sendMessageToWebhook(type, message) {
   const webhook =
     type === "ERROR"
       ? import.meta.env.VITE_HOOK_SUPA
       : import.meta.env.VITE_HOOK_REVIEW;
   try {
-    const response = await fetch(webhook, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(message),
+    user.subscribe(async (user) => {
+      const response = await fetch(webhook, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          content: JSON.stringify({
+            Error: message,
+            User: user,
+          }),
+        }),
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      console.log("Successfully sent message to webhook");
     });
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    console.log("Successfully sent message to webhook");
   } catch (err) {
     console.error(`Error sending message to webhook: ${err}`);
   }
