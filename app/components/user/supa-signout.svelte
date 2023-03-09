40549bd75e863abbb89278a036cb11bc5ff03f91
SinoLewis <sinolewis@gmail.com>
2023-03-09 13:40:23 +0300

    Styling

--- app/components/user/supa-signout.svelte
+++ app/components/user/supa-signout.svelte
@@ -8,6 +8,6 @@
 
 <style lang="scss">
   button {
-    @apply btn cursor-pointer px-4 py-2 text-xl font-display text-white bg-white hover:bg-purple-600 drop-shadow-[6px_6px_0_black] hover:drop-shadow-[0_0_7px_rgba(168,85,247,0.5)] transition-all duration-300;
+    @apply btn cursor-pointer px-4 py-2 text-xl font-display text-white bg-black hover:bg-purple-600 drop-shadow-[6px_6px_0_black] hover:drop-shadow-[0_0_7px_rgba(168,85,247,0.5)] transition-all duration-300;
   }
 </style>
