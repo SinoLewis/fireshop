91099c7d600beb50f1b775c74ae059879680383a
SinoLewis <sinolewis@gmail.com>
2023-05-25 20:26:36 +0300

    User id subscription for stores

--- app/stores/supa-user.ts
+++ app/stores/supa-user.ts
@@ -1,7 +1,7 @@
 import { writable } from "svelte/store";
 import type { User } from "@supabase/supabase-js";
-import { supabase } from "../util/supabase";
-import { cart } from "./cart";
+import { supabase } from "../util/supabase.auth";
+import { cart, destination, order } from "./";
 
 const user = writable<User>(null);
 
@@ -15,11 +15,20 @@ async function getUser() {
   try {
     const { data, error } = await supabase.auth.getUser();
     if (error) throw error;
-    cart.update((value) => {
-      value.user_id = data.user ? data.user.id : null;
-      return { ...value };
-    });
     user.set(data.user);
+    cart.update((c) => {
+      c.user_id = data.user.id;
+      return { ...c };
+    });
+    destination.update((d) => {
+      d.user_id = data.user.id;
+      return { ...d };
+    });
+    order.update((o) => {
+      o.user_id = data.user.id;
+      o.email = data.user.email;
+      return { ...o };
+    });
   } catch (error) {
     user.set(null);
     console.log("USER ERROR: ", error.message);
