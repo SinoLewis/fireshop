3e2b0960d7722040e2dbcc5b7fe493428484e665
SinoLewis <sinolewis@gmail.com>
2023-05-25 20:59:07 +0300

    Testin update

--- app/components/shop/testin.svelte
+++ app/components/shop/testin.svelte
@@ -1,104 +1,28 @@
 <svelte:options tag="testin-button" />
 
 <script lang="ts">
-  import { supabase } from "../../util/supabase";
   import { onMount } from "svelte";
+  import { user } from "../../stores";
 
-  let addressEl: HTMLInputElement;
+  let src = "/img/ui/avatar.svg";
+  // let user_img; // Assuming $user is a Svelte store
 
-  const edgeTest = async () => {
-    const body = {
-      start: "8.681495,49.41461",
-      end: "8.687862,49.420318",
-    };
-    try {
-      const { data, error } = await supabase.functions.invoke(
-        "ors-directions",
-        {
-          body: JSON.stringify(body),
-        }
-      );
-      if (error) throw error;
-      return data;
-    } catch (error) {
-      console.warn("Edge Testin Error", error);
-    }
-  };
-  onMount(async () => {
-    console.log("Testin", await edgeTest());
-  });
+  // onMount(() => {
+  //   // Simulate API call
+  //   setTimeout(() => {
+  //     // Update the $user store with the API response
+  //     $user_img = {
+  //       user_metadata: {
+  //         avatar_url: "https://example.com/avatar.jpg",
+  //       },
+  //     };
+  //   }, 2000); // Delay for 2 seconds, replace with your actual API call
+  // });
 
-  function debounce(func, delay) {
-    let timeoutId;
-    return function (...args) {
-      clearTimeout(timeoutId);
-      timeoutId = setTimeout(() => {
-        func.apply(this, args);
-      }, delay);
-    };
+  $: {
+    // Reactive assignment to update src when $user changes
+    src = $user?.user_metadata?.avatar_url ?? "/img/ui/avatar.svg";
   }
-
-  // Throttle function that limits the number of requests sent per second to 5
-  function throttle(func, limit) {
-    let inThrottle;
-    return function (...args) {
-      if (!inThrottle) {
-        inThrottle = true;
-        func.apply(this, args);
-        setTimeout(() => {
-          inThrottle = false;
-        }, limit);
-      }
-    };
-  }
-
-  let index = 0;
-
-  async function getAddressHits(query) {
-    try {
-      // fetch(`http://localhost:3000/geocode/${query}`)
-      fetch(`http://localhost:3000/directions/${query}`)
-        .then((response) => response.json())
-        .then((data) => console.log(data))
-        .catch((error) => console.error(error));
-      console.log(`API REQ NO: ${query}, ${index++}`);
-    } catch (error) {
-      console.log("OpenStreetMap ERROR: ", error);
-    }
-  }
-  const debouncedSearch = debounce(getAddressHits, 2500);
-  const throttledSearch = throttle(getAddressHits, 2000);
-
-  const handleInput = (e: Event) => {
-    const q = (e.target as HTMLInputElement).value;
-    debouncedSearch(q);
-  };
 </script>
 
-<h1>Testin: Limiited Cached API request</h1>
-<form on:submit|preventDefault>
-  <label for="address">
-    <span>Debounced</span>
-    <input
-      class="input-field"
-      type="text"
-      name="address"
-      placeholder="Address"
-      bind:this={addressEl}
-      on:input={handleInput}
-      required
-    />
-  </label>
-  <label for="address">
-    <span>Throttle</span>
-    <input
-      class="input-field"
-      type="text"
-      name="address"
-      placeholder="Address"
-      bind:this={addressEl}
-      on:input={throttledSearch}
-      required
-    />
-  </label>
-</form>
+<img {src} alt="User Avatar" />
