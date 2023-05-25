4cde75e7d772d79cd0329d5d4ad239ee278031fd
SinoLewis <sinolewis@gmail.com>
2023-05-25 20:15:16 +0300

    supabase util functions

---
+++ app/util/supabase.auth.ts
@@ -0,0 +1,76 @@
+import { createClient, OAuthResponse } from "@supabase/supabase-js";
+import { toast, modal } from "../stores";
+import { sendMessageToWebhook } from "./discord";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+async function loginHandler(promise: Promise<OAuthResponse>) {
+  let res: any, serverError: string;
+  try {
+    res = await promise;
+    modal.set(null);
+    toast.set({
+      message: "Access granted! Logged into the mainframe!",
+      type: "success",
+    });
+  } catch (err) {
+    serverError = err.message;
+    console.error(err);
+    toast.set({
+      message: serverError,
+      type: "error",
+    });
+  }
+  return { res, serverError };
+}
+
+export async function signInWithGoogle() {
+  let credential = supabase.auth.signInWithOAuth({
+    provider: "google",
+    options: {
+      redirectTo: `${window.location.origin}/${window.location.pathname}`,
+    },
+  });
+  return loginHandler(credential);
+}
+
+export async function signInWithApple() {
+  let credential = supabase.auth.signInWithOAuth({
+    provider: "apple",
+    options: {
+      redirectTo: `${window.location.origin}/${window.location.pathname}`,
+    },
+  });
+  return loginHandler(credential);
+}
+
+export async function supabaseSignOut() {
+  let { error } = await supabase.auth.signOut();
+  if (error) sendMessageToWebhook("ERROR", error.message);
+  toast.set({
+    icon: error ? "❌" : "🫶",
+    message: error ? error.message : "Thanks for hanging out, see ya around!",
+  });
+}
+
+export async function passwordlessSignin(email: string) {
+  let res: any, serverError: any;
+  let { data, error } = await supabase.auth.signInWithOtp({
+    email: email,
+    options: {
+      emailRedirectTo: `${window.location.origin}/${window.location.pathname}`,
+    },
+  });
+  serverError = error;
+  // TEST
+  // console.log("SERVER DATA: ", data);
+  // console.log("SERVER ERROR: ", data);
+  if (!error) {
+    res = `Magic signin link sent to ${email}`;
+    sendMessageToWebhook("AUTH", `${email} requested a login link`);
+  }
+  return { res, serverError };
+}
---
+++ app/util/supabase.db.ts
@@ -0,0 +1,186 @@
+import type { Cart, Order, CartProducts, Destination } from "../stores";
+import { supabase } from "./supabase.auth";
+import { sendMessageToWebhook } from "./discord";
+
+async function getDestination(id: string) {
+  const { data, error } = await supabase
+    .from("destinations")
+    .select("*")
+    .eq("id", id);
+  console.log("DESTINATION get: ", data[0]);
+  if (error) throw error;
+  return data[0];
+}
+
+async function insertDestination(db_destination: object) {
+  const { data, error }: { data: any; error: any } = await supabase
+    .from("destinations")
+    .insert(db_destination)
+    .select();
+  if (error) throw error;
+  return data[0];
+}
+
+export async function updateDestination(destination: Destination) {
+  try {
+    const user_destination = await getDestination(destination.id);
+    if (
+      user_destination?.user_id === destination.user_id &&
+      user_destination?.id === destination.id
+    ) {
+      const { data, error }: { data: any; error: any } = await supabase
+        .from("destinations")
+        .update(destination)
+        .eq("id", destination.id)
+        .select();
+      if (error) throw error;
+      console.log("DESTINATION update DB: ", data);
+      return data[0];
+    } else {
+      const data = await insertDestination(destination);
+      console.log("DESTINATION insert DB: ", data);
+      return data;
+    }
+  } catch (error) {
+    console.log("DESTINATION update ERROR: ", error);
+    sendMessageToWebhook("ERROR", error.message);
+  }
+}
+
+async function getCustomOrders(order: Order) {
+  const { data, error } = await supabase
+    .from("custom-orders")
+    .select("*")
+    .eq("id", order.id);
+  if (error) throw error;
+  return data[0];
+}
+
+async function insertCustomOrders(order: Order) {
+  const { data, error }: { data: any; error: any } = await supabase
+    .from("custom-orders")
+    .insert(order)
+    .select();
+  if (error) throw error;
+  return data[0];
+}
+
+export async function updateOrder(order: Order): Promise<Order> {
+  const date = new Date();
+  try {
+    const user_order = await getCustomOrders(order);
+    //  TEST
+    console.log("ORDER get DB: ", user_order);
+    if (user_order?.user_id === order.user_id && user_order?.id === order.id) {
+      const { data, error }: { data: any; error: any } = await supabase
+        .from("custom-orders")
+        .update(order)
+        .eq("id", order.id)
+        .select();
+      if (error) throw error;
+      console.log("ORDER update DB: ", data);
+      return data[0];
+    } else {
+      const data = await insertCustomOrders(order);
+      console.log("ORDER insert DB: ", data);
+      return data;
+    }
+  } catch (error) {
+    console.log("ORDER update ERROR: ", error);
+    sendMessageToWebhook("ERROR", error.message);
+  }
+}
+
+async function getPriceById(id) {
+  try {
+    const { data, error } = await supabase
+      .from("products")
+      .select("*")
+      .eq("id", id);
+    if (error) throw error;
+    // TEST
+    // console.log("PRICE: ", data[0]["price"]);
+    return data[0]["price"];
+  } catch (error) {
+    console.error("PRICE ERROR", error.message);
+    sendMessageToWebhook("ERROR", error.message);
+  }
+}
+
+async function getCarts(cart: Cart) {
+  const { data, error } = await supabase
+    .from("carts")
+    .select("*")
+    .eq("id", cart.id);
+  if (error) throw error;
+  return data[0];
+}
+
+async function insertCarts(db_cart, cart_products: CartProducts) {
+  const { data, error }: { data: any; error: any } = await supabase
+    .from("carts")
+    .insert({
+      ...db_cart,
+      cart_products: Object.values(cart_products).map((v) => v.id),
+    })
+    .select();
+  if (error) throw error;
+  return data[0];
+}
+
+export async function updateCart(cart: Cart): Promise<Cart> {
+  Object.values(cart.cart_products).forEach(
+    async (item) => (item.price = await getPriceById(item.id))
+  );
+  const { cart_products, ...db_cart } = cart;
+  // TODO: Test if its right price with fake html price
+  try {
+    const user_cart = await getCarts(cart);
+    // TEST
+    console.log("CART get DB: ", user_cart);
+    if (user_cart?.user_id === cart.user_id && user_cart?.id === cart.id) {
+      const { data, error }: { data: any; error: any } = await supabase
+        .from("carts")
+        .update({
+          ...db_cart,
+          cart_products: Object.values(cart_products).map((v) => v.id),
+          // id: undefined,
+        })
+        .eq("id", cart.id)
+        .select();
+      if (error) throw error;
+      console.log("CART update DB: ", data);
+      return data[0];
+    } else {
+      const data = await insertCarts(db_cart, cart_products);
+      console.log("CART insert DB: ", data);
+      return data;
+    }
+  } catch (error) {
+    console.log("CART update ERROR: ", error);
+    sendMessageToWebhook("ERROR", error.message);
+  }
+}
+
+async function updateProductQuantity(cart: Cart) {
+  Object.values(cart.cart_products).forEach(async (item) => {
+    try {
+      const { data: product, error: p_error } = await supabase
+        .from("products")
+        .select("*")
+        .eq("id", item.id);
+      if (p_error) throw p_error;
+      product[0]["quantity"] - item.quantity;
+      const { data, error } = await supabase
+        .from("products")
+        .update({ ...product })
+        .eq("id", item.id);
+      if (error) throw error;
+      // TEST
+      //  console.log("PRODUCTS update: ", data);
+    } catch (error) {
+      console.log("PRODUCTS update ERROR: ", error);
+      sendMessageToWebhook("ERROR", error.message);
+    }
+  });
+}
